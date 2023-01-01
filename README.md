1becc4809bd52dfc693316329930481557f83ce7
Fernando R. B <RBFernando99@gmail.com>
2023-01-02 00:53:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ponos-backend
+ This is the backend of Ponos, a HR software.
---
+++ src/main/java/com/rbfernando99/Ponos/PonosApplication.java
@@ -0,0 +1,13 @@
+package com.rbfernando99.Ponos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PonosApplication.class, args);
+	}
+
+}
